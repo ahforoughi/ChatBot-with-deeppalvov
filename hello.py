f05fc28a5eeece982909512cdf2bd2a8704b26b5
AMHF99 <47900767+AMHF99@users.noreply.github.com>
2020-02-06 12:22:37 +0330

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+from deeppavlov.skills.pattern_matching_skill import PatternMatchingSkill
+from deeppavlov.agents.default_agent.default_agent import DefaultAgent
+from deeppavlov.agents.processors.highest_confidence_selector import HighestConfidenceSelector
+hello = PatternMatchingSkill(responses=['Hello world!'], patterns=["hi", "hello", "good day"])
+bye = PatternMatchingSkill(['Goodbye world!', 'See you around'],
+                            patterns=["bye", "chao", "see you"])
+fallback = PatternMatchingSkill(["I don't understand, sorry", 'I can say "Hello world!"'])
+agent = DefaultAgent([hello, bye, fallback], skills_selector=HighestConfidenceSelector())
+agent(['Hello', 'Bye', 'Or not'])
